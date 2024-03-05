003f9714410cfb0f60f235f1ad8f0dc33a72d3c1
miguel <jantsag774@g.educaand.es>
2024-03-05 19:25:04 +0100

    Agregados comentarios de figura

--- Figura.java
+++ Figura.java
@@ -1,5 +1,21 @@
+/**
+*Representa la estructura de figuras geométricas.
+*Las subclases deben implementar los métodos para calcaular área y perímetro
+*
+*@autor miguel
+*/
 public abstract class Figura {
+   /**
+   *Retorna el área de la figura
+   *
+   *@return Área como double.*/
+    
     public abstract double area();
+    /**
+    *Retorna el perímetro de la figura.
+    *
+    *@return Perímetro como double.
+    */
     public abstract double perimetro();
 }
 
